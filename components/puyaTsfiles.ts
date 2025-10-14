66a411f0fd192e0e3ee2b6a68aab5e6146e981a4
nickthelegend <testingtesla7@gmail.com>
2025-10-14 10:00:27 +0530

    TEST

--- components/puyaTsfiles.ts
+++ components/puyaTsfiles.ts
@@ -1,35 +1,37 @@
+import { dir } from "console";
+
 export const puyaTsfiles = {
-  "helloworld.algo.ts": {
+  smart_contracts :{
+    directory:{
+
+      "helloworld.algo.ts": {
     file: {
       contents: `import { BaseContract, log, op } from '@algorandfoundation/algorand-typescript'
 
 export default class HelloWorldContract extends BaseContract {
   public approvalProgram(): boolean {
     const name = String(op.Txn.applicationArgs(0))
-    log("Hello," + name)
+    log("Hello")
     return true
   }
-}`,
+}
+`,
     },
   },
+    }
+  }
+  
+  ,
   "package.json": {
     file: {
       contents: `{
-  "name": "puyats-example",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "build": "algokit compile",
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "keywords": [],
-  "author": "",
-  "license": "ISC",
   "dependencies": {
     "@algorandfoundation/algorand-typescript": "^1.0.0"
+        "@algorandfoundation/puya-ts": "^1.0.0"
+
   }
-}`,
+}
+`,
     },
   },
 };
